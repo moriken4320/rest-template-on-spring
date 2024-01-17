24069f0a9258b6abaea881e2986e6e16c48e7edd
moriken4320 <moriken.4320@gmail.com>
2024-01-17 11:54:07 +0900

    first commit

---
+++ src/main/kotlin/com/example/resttemplateonspring/RestTemplateOnSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.example.resttemplateonspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestTemplateOnSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<RestTemplateOnSpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/resttemplateonspring/RestTemplateOnSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.resttemplateonspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestTemplateOnSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
